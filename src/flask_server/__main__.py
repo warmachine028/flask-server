33354a590f2c4bf5626ab131e204dc4f88527baa
warmachine028 <warmachine028@outlook.com>
2023-09-25 00:06:48 +0530

    remove: vercel

--- src/flask_server/__main__.py
+++ src/flask_server/__main__.py
@@ -3,7 +3,8 @@ from flask_server import create_app
 app = create_app()
 
 def main():
-    app.run(host="localhost", debug=True)
+    # app.run(host="localhost", debug=True)
+    app.run(host="0.0.0.0")
 
 @app.route("/")
 def hello_world():
