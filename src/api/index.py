2c20090dd6fad6cfa1a724dd97a6bc560ed4722b
warmachine028 <warmachine028@outlook.com>
2023-09-25 14:27:59 +0530

    add: api/index

--- src/api/index.py
+++ src/api/index.py
@@ -1,17 +1,29 @@
-from importlib import metadata
-from api import create_app
+# from importlib import metadata
+# from api import create_app
 
-__version__ = metadata.version("flask_server")
+# __version__ = metadata.version("flask_server")
 
-app = create_app()
+# app = create_app()
 
-def main():
-    # app.run(host="localhost", debug=True)
-    app.run(host="0.0.0.0")
+# def main():
+#     # app.run(host="localhost", debug=True)
+#     app.run(host="0.0.0.0")
 
-@app.route("/")
-def hello_world():
-    return "<h1>Hello from flask Server</h1>"
+# @app.route("/")
+# def hello_world():
+#     return "<h1>Hello from flask Server</h1>"
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello, World!'
+
+@app.route('/about')
+def about():
+    return 'About'
