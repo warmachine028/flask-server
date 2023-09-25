c60d84b3ef7c375e56d62f1d9678e1468804f247
warmachine028 <warmachine028@outlook.com>
2023-09-25 13:51:46 +0530

    add: vercel

--- src/flask_server/index.py
+++ src/flask_server/index.py
@@ -1,19 +1,19 @@
 from importlib import metadata
 from flask import Flask
-from flask_server.routes.health import health_bp
-from flask_server.routes.users import users_bp
-from flask_server.routes.error import error_bp
-from flask_server.routes.auth import auth_bp
+# from flask_server.routes.health import health_bp
+# from flask_server.routes.users import users_bp
+# from flask_server.routes.error import error_bp
+# from flask_server.routes.auth import auth_bp
 
 __version__ = metadata.version("flask_server")
 
 
 # def create_app():
 app = Flask(__name__)
-app.register_blueprint(health_bp)
-app.register_blueprint(users_bp)
-app.register_blueprint(error_bp)
-app.register_blueprint(auth_bp)
+# app.register_blueprint(health_bp)
+# app.register_blueprint(users_bp)
+# app.register_blueprint(error_bp)
+# app.register_blueprint(auth_bp)
     # return app
 
 
