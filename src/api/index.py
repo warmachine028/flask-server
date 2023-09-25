6a28fefaf120f8047407692f52ea3cc0a083dd88
warmachine028 <warmachine028@outlook.com>
2023-09-25 14:38:03 +0530

    refactor: api/index

--- src/api/index.py
+++ src/api/index.py
@@ -3,27 +3,19 @@
 
 # __version__ = metadata.version("flask_server")
 
-# app = create_app()
-
-# def main():
-#     # app.run(host="localhost", debug=True)
-#     app.run(host="0.0.0.0")
-
-# @app.route("/")
-# def hello_world():
-#     return "<h1>Hello from flask Server</h1>"
-
-# if __name__ == "__main__":
-#     main()
-
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
 
-@app.route('/about')
-def about():
-    return 'About'
+def main():
+    app.run(host="localhost", debug=True)
+
+
+@app.route("/")
+def hello_world():
+    return "<h1>Hello from flask Server</h1>"
+
+
+if __name__ == "__main__":
+    main()
