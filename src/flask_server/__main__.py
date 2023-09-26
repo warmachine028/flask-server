5f34099827d83d30cf41976cccc81cd4fe0eae41
warmachine028 <warmachine028@outlook.com>
2023-09-27 01:03:26 +0530

    feat: port number

--- src/flask_server/__main__.py
+++ src/flask_server/__main__.py
@@ -1,9 +1,10 @@
 from flask_server import create_app
+import os
 
 app = create_app()
 
 def main():
-    app.run(debug=True)
+    app.run(port=os.environ.get('PORT', 5000))
 
 @app.route("/")
 def hello_world():
