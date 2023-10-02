1fe30fd6d890d4c5d5b661ceaeefd44086ff2306
warmachine028 <warmachine028@outlook.com>
2023-10-02 14:39:50 +0530

    fix: added fixes for vercel.json 4

--- src/flask_server/__main__.py
+++ src/flask_server/__main__.py
@@ -1,15 +1,24 @@
-from flask_server import create_app
-import os
+# from flask_server import create_app
+# import os
 
-app = create_app()
+# app = create_app()
 
-def main():
-    # app.run(port=os.environ.get('PORT', 3000))
-    app.run(port="localhost:5000")
+# def main():
+#     # app.run(port=os.environ.get('PORT', 3000))
+#     app.run(port="localhost:5000")
+
+# @app.route("/")
+# def hello_world():
+#     return "<h1>Hello from flask Server</h1>"
+
+# if __name__ == "__main__":
+#     main()
+
+from flask import Flask
+
+app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<h1>Hello from flask Server</h1>"
 
-if __name__ == "__main__":
-    main()
